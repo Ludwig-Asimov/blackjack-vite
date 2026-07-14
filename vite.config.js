f040419061e196cc9fb01289024cafb72bcdbe47
Ludwig Asimov <215780582+Ludwig-Asimov@users.noreply.github.com>
2026-07-14 12:50:52 -0600

    Vite: Se configuró el prefijo 'base' para las URLs en el build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/blackjack-vite/",       // prefijo para todas las URLs en el build final
+
+  build: {
+    outDir: "docs",               // Cambia el nombre de la carpeta de salida (por defecto es dist).
+    emptyOutDir: true,            // Asegura que la carpeta se vacíe antes de generar un nuevo build, evitando archivos viejos mezclados.
+  },
+
   server: {
-    host: true,   // escucha en 0.0.0.0
+    host: true,                   // escucha en 0.0.0.0 (necesario en contenedor)
     port: 5173,
     watch: {
       usePolling: true
     }
   }
-}
+});
